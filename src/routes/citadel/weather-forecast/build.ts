ccbc4a23e895d91a2042884eb536569dfb5e738b
naianeyanachi <naiane.yanachi@gmail.com>
2024-10-02 16:40:03 -0300

    route get weather forecast

---
+++ src/routes/citadel/weather-forecast/build.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import db from '../../../models'
+
+export const build = async (req: Request, res: Response) => {
+  try {
+    const citadel = await db.Citadel.getCitadel(db, req.params.id)
+    if (!citadel) {
+      return res.status(404).json({ message: 'Citadel not found' })
+    }
+
+    await citadel.updateCitadel(db)
+    // TODO: build weather forecast
+
+    const updatedCitadel = await db.Citadel.getCitadel(db, req.params.id)
+    return res.json(updatedCitadel)
+  } catch (error: unknown) {
+    console.error('Error starting factory:', error)
+    if (error instanceof Error) {
+      res
+        .status(400)
+        .json({ message: 'Failed to start factory', error: error.message })
+    } else {
+      res.status(400).json({
+        message: 'Failed to start factory',
+        error: 'An unknown error occurred',
+      })
+    }
+  }
+}
