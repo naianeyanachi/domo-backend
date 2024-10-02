32e7eea7259e30c222af68ff3e2ac74ecf30140b
naianeyanachi <naiane.yanachi@gmail.com>
2024-10-02 16:38:23 -0300

    return builds possibilities by the req structure

--- src/models/structure-requirement.ts
+++ src/models/structure-requirement.ts
@@ -1,19 +1,24 @@
 'use strict'
 import { Model, DataTypes, Sequelize } from 'sequelize'
+import { Structure } from './structure'
 
 export class StructureRequirement extends Model {
   public idStructure!: number
   public idRequiredStructure!: number
   public level!: number
+  public structure?: Structure | null
+  public requiredStructure?: Structure | null
 
   static associate(models: any) {
     StructureRequirement.hasOne(models.Structure, {
       foreignKey: 'id',
       sourceKey: 'idStructure',
+      as: 'structure',
     })
     StructureRequirement.hasOne(models.Structure, {
       foreignKey: 'id',
       sourceKey: 'idRequiredStructure',
+      as: 'requiredStructure',
     })
   }
 }
@@ -24,10 +29,12 @@ export default (sequelize: Sequelize) => {
       idStructure: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        primaryKey: true,
       },
       idRequiredStructure: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        primaryKey: true,
       },
       level: {
         type: DataTypes.INTEGER,
